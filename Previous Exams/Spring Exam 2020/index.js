8dc1f73c026e6705d28d9e6f7a2c271220849175
ipeglin <philip@eglin.no>
2020-12-13 01:23:55 +0100

    Improved the exam solution. Found a better solution to assignment 3

--- Previous Exams/Spring Exam 2020/index.js
+++ Previous Exams/Spring Exam 2020/index.js
@@ -1,226 +1,235 @@
 // Oppgave 1
 
+
 // a
-function OPG1a() {
-    let numberLimit = document.getElementById("OPG1aInput").value;
-    let Output = document.getElementById("OPG1aOutput");
+
+// Adding up all interger between 1 and given number n
+function sumWithLimiter() {
+    let iterationLimit = document.getElementById("intStartInput").value;
     let sum = 0;
 
-    // Checking if input is NaN
-    if (isNaN(numberLimit)) {
-        // Display error message
-        Output.innerHTML = "<i style='color:red'>Invalid input</i>";
-    }
-    else {
-        // Summing all numbers between 1 and n
-        for (let i = 1; i <= numberLimit; i++) {
+    // Checking if input is a number
+    if (!isNaN(iterationLimit)) {
+        // Flooring number down in case n is a floating number
+        iterationLimit = Math.floor(iterationLimit);
+
+        // Adding all integers from 1 to n
+        for (let i = 0; i < iterationLimit; i++) {
             sum += i;
-        }
-        // Display results
-        Output.innerText = "Sum: " + sum;
+        }    
+        // Display for user
+        document.getElementById("OPG1aOutput").innerText = "Sum: " + sum;
     }
 }
 
+
 // b
-function OPG1b() {
-    let numberLimit = document.getElementById("OPG1bInput").value;
-    let Output = document.getElementById("OPG1bOutput");
+
+// Adding all integers from 1 to n which are divisable by 3 or 5 
+function sumWithModulo() {
+    let iterationLimit = document.getElementById("intStartInput2").value;
     let sum = 0;
 
-    // Cheching if input is NaN
-    if (isNaN(numberLimit)) {
-        // Display error message
-        Output.innerHTML = "<i style='color:red'>Invalid input</i>";
-    }
-    else {
-        // Summing all numbers between 1 and n which also are divisable by 3 or 5
-        for (let i = 0; i <= numberLimit; i++) {
+    // Checking if input is a number
+    if (!isNaN(iterationLimit)) {
+        // Flooring number down in case n is a floating number
+        iterationLimit = Math.floor(iterationLimit);
+
+        // Adding all integers from 1 to n if the meet requirements
+        for (let i = 0; i < iterationLimit; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 sum += i;
             }
-            else {
-                continue;
-            }
         }
-        // Display results
-        Output.innerText = "Sum: " + sum;
+
+        // Display for user
+        document.getElementById("OPG1bOutput").innerText = "Sum: " + sum;
     }
 }
+
 
 // c
-let count = 0;
-function OPG1cMain() {
-    let button = document.getElementById("OPG1cSubmit");
-    let guess = document.getElementById("OPG1cInput").value;
 
-        // Cheching if input is NaN
-    if (isNaN(guess)) {
-        OPG1cResults("NaN");
-    }
-    else {
-        // Run feedback function with the guessed number as argument
-        OPG1cFeedback(guess);
-        // Increment the counter
-        count++;
-    }
-}
+let guesses = 0;
 
-function OPG1cFeedback(guess) {
-    let goal = 17;
-    // Comparing the guess to the targeted number
-    // Calling on results function with respective agruments
-    if (guess == goal) {
-        OPG1cResults("eq");
-    }
-    else if (guess > goal) {
-        OPG1cResults("gt");
-    }
-    else {
-        OPG1cResults("lt");
-    }
-
-}
-
-function OPG1cResults(arg) {
+function submitGuess() {
+    let number = document.getElementById("numberGuess").value;
     let output = document.getElementById("OPG1cOutput");
-    // Determining output string depending on the parameters given
-    switch (arg) {
-        case "eq":
-            output.innerText = "Congratulations! You guessed " + count + " times!";
-            break;
-        case "gt":
-            output.innerText = "Your guess is greater than the secret number";
-            break;
-        case "lt":
-            output.innerText = "Your guess is less than the secret number";
-            break;
-        default:
-            output.innerHTML = "<i style='color:red'>Invalid input</i>";
+
+    // Clearing div if input is empty for a cleaner look
+    if (guesses == 0) {
+        output.innerText = "";
+    }
+
+    // Increment amount of guesses
+    guesses++;
+
+    // Checking if input is a number
+    if (!isNaN(number)) {
+        // Checking if the number is correct
+        if (checkNumber(number)) {
+            output.innerText = "Gratulerer! Du brukte " + guesses + " forsøk";
+            guesses = 0;
+        }
+    }
+    else {
+        output.innerText = "Invalid input";
+    }
+}
+
+// Comparing guessed number to hard coded number goal
+function checkNumber(num) {
+    if (num == 15) {
+        return true;
+    }
+    else {
+        return false;
     }
 }
 
 
 // Oppgave 2
 
+
 // a
 
-function OPG2a() {
-    let string = document.getElementById("OPG2aInput").value;
+function isPalindrome() {
+    let word = document.getElementById("wordInput").value;
     let output = document.getElementById("OPG2aOutput");
 
-    let stringArr = [];
-    let reverseString = "";
+    // Checking if the input is a string
+    if (typeof (word) == "string") {
+        let stringArray = [];
+        let inverse = "";
 
-    // Append all characters from string to array in reversed order
-    for (let character of string) {
-        stringArr.unshift(character);
-    }
-    
-    // Constructing new string which is a reverse of original input string
-    for (let element of stringArr) {
-        reverseString += element;
-    }
-    
-    // Comparing the two strings
-    if (string == reverseString && string != "") {
-        output.innerText = string + " er et palindrom";
-    }
-    else {
-        output.innerText = string + " er IKKE et palindrom";
+        // Appending every letter in word to array in reversed order
+        for (let element of word) {
+            stringArray.unshift(element);
+        }
+
+        // Appending every letter of reversed array to the inverse string
+        for (let element of stringArray) {
+            inverse += element;
+        }
+
+        // Displaying result to user. If input is empty, make a clean sweep of div
+        if (word.length > 0) {
+            let bool = word == inverse ? "Ja" : "Nei";
+            output.innerText = "Er '" + word + "' et palidrom: " + bool;
+        }
+        else {
+            output.innerText = "";
+        }
+        
     }
 }
+
 
 // b
 
-function OPG2b() {
-    let isLeapYear = false;
-
-    let year = document.getElementById("OPG2bInput").value;
+function checkLeapYear() {
+    let year = document.getElementById("yearInput").value;
     let output = document.getElementById("OPG2bOutput");
 
-
     let lastTwoDigits = "";
-    // Setting the last two digits of input year
-    lastTwoDigits += year[year.length - 2] + year[year.length - 1];
+    let isLeapYear = false;
 
-    // Cheching if year is a new century
-    if (lastTwoDigits == "00") {
-        // Checking if year is a leap year
-        if (year % 400 == 0) {
-            isLeapYear = true;
+    // Checking if input is a number
+    if (!isNaN(year)) {
+        // If year has two digits or more
+        if (year >= 10 || year <= -10) {
+            lastTwoDigits += year[year.length - 2] + year[year.length - 1]
         }
-    }
-    else {
-        // Checking if year is a leap year
-        if (year % 4 == 0) {
-            isLeapYear = true;
-        }
-    }
 
-    // Cheching if year is NaN
-    if (isNaN(year)) {
-        output.innerHTML = "<i style='color:red'>Invalid input</i>";
-    }
-    // If not NaN. Checking if it is a leap year or not
-    else if (isLeapYear) {
-        output.innerText = year + " er et skuddår";
-    }
-    else {
-        output.innerText = year + " er IKKE et skuddår";
+        // If the last two digits of a year is 00
+        if (lastTwoDigits == "00") {
+            // Check if it is a leap year
+            if (year % 400 == 0) {
+                isLeapYear = true;
+            }
+        }
+        else {
+            // If the year is not the start of a century. Check if it is a leap year
+            if (year % 4 == 0) {
+                isLeapYear = true;
+            }
+        }
+
+        // Display to user if the year is a leap year or not. If input is empty, clean div
+        if (isLeapYear) {
+            output.innerText = year + " er et skuddår.";
+        }
+        else {
+            output.innerText = year + " er IKKE et skuddår.";
+        }
     }
 }
 
+
 // Oppgave 3
 
+
+// RGB value
 let x = 0;
-let num = 1;
 
-// Setting interval to start when loading page
-setInterval(() => {
-    // Setting background color to black
-    document.body.style.backgroundColor = 'rgb(' + x + "," + x + "," + x + ')';
+// Number to increment
+let iteration = 1;
 
-    // Increase rgb value by 5 every interval iteration
+// Set interval to run on load
+let colorInterval = setInterval(() => {
+
+    // Change the background color of the page to black
+    document.body.style.backgroundColor = "rgb(" + x + "," + x + "," + x + ")";
+
+    // Increment the RGB value by 5 every iteration
     x += 5;
 
-    // When background is completely white; do...
-    if (x == 255) {
-        // Setting new interval for numbers
-        setInterval(() => {
-            // Creating new div element
-            let div = document.createElement("div");
-            // Setting innerText to number value for interval iteration
-            div.innerText = num;
-            
-            // Setting style for new div element
-            div.style.position = "absolute";
-            div.style.left = Math.floor(Math.random() * 100);
-            div.style.top = 100 + Math.floor(Math.random() * 100);
+    // When the page's background is white
+    if (x >= 255) {
+        // Stop the first interval
+        clearInterval(colorInterval);
 
-            // Appending element to body
+        // Set new interval for divs
+        setInterval(() => {
+            // Creating div element
+            let div = document.createElement("div");
+            
+            // Setting content of div
+            div.innerText = iteration;
+
+            // Setting style for div
+            div.style.position = "absolute";
+            div.style.left = Math.floor(Math.random() * 300) + "px";
+            div.style.top = 100 + Math.floor(Math.random() * 300) + "px";
+
+            // Choosing a grey color for numbers so its will be easier to check the other assignments
+            div.style.color = "rgb(175,175,175)"
+
+            // Appending child div to body
             document.body.appendChild(div);
 
-            // Increment number for next iteration
-            num++;
-        }, 500); // Do every 0.5 seconds
+            // Incrementing iteration number for the next looparound
+            iteration++;
+
+        }, 500); // Do this twice every second
     }
-}, 20); // Do every 0.02 seconds
+}, 20); // Repeat every 20 milliseconds until background is white
 
 
 // Oppgave 4
 
 let v = [2, 4, 6];
 
-// a
+// Multiplying every element with the index of itself
 console.log("each element of v multiplied by its index: [" + v.map((element) => element * v.indexOf(element)) + "]");
 
-// b
-console.log("2 multiplied by the sum of v: " + (v.reduce((accu, element) => accu + element, 0)) * 2);
+// Multiplying the total sum of all elements in array by two
+console.log("2 multiplied by the sum of v: " + (v.reduce((accu, element) => accu + element, 0) * 2));
 
-// c
-console.log("all elements of v are positive numbers: ", v.every((element) => element > 0) ? "yes" : "no");
+// Checking if all the elements in the array are positive
+console.log("all elements of v are positive numbers: ", v.every((element) => element > 0) ? "Yes" : "No");
 
-// d
+
 
 let patients = [
     { name: "Ola", hasFever: false },
@@ -228,13 +237,14 @@ let patients = [
     { name: "Knut", hasFever: true }
 ];
 
-console.log("patrients who have fever: [", patients.map((patient) => patient.hasFever ? patient.name : null).filter((patient) => patient != null), "]");
+// Displaying the name of all patients with a fever
+console.log("patients who have fever: [", patients.map((patient) => patient.hasFever ? patient.name : "").filter((name) => name != "") + "]");
 
-// e
-console.log("Number of patients with fever: ", patients.map((patient) => patient.hasFever).filter((bool) => bool).length);
+// Displaying the amount of patients who havs a fever
+console.log("Number of patrients with fever: ", (patients.map((patient) => patient.hasFever ? patient.name : "").filter((name) => name != "").length));
 
-// f
-console.log("First patient who has fever in the patients table: ", patients.map((patient) => patient.hasFever ? patient.name : null).filter((bool) => bool != null)[0]);
+// Finding the first patient in the array who has a fever
+console.log("First patient who has fever is the patients table: ", patients.map((patient) => patient.hasFever ? patient.name : "").filter((name) => name != "")[0]);
 
-// g
+// Checking if the patient named Ola has a fever
 console.log("Does Ola have fever: ", patients.find((patient) => patient.name == "Ola").hasFever ? "Yes" : "No");
